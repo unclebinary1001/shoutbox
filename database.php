981ff4ae729c690b0eae61476adcde59f53972e0
Mahlangu Nzunda <nzundamahlangu@gmail.com>
2021-01-19 16:46:35 +0200

    change server information in database file

--- database.php
+++ database.php
@@ -1,6 +1,9 @@
 <?php
 //Connect to MySQL
-$con = mysqli_connect("localhost","root",'',"shoutit");
+//$con = mysqli_connect("localhost","root",'',"shoutit");
+
+//Remote database connection
+$con = mysqli_connect("remotemysql.com","NhQd82hxIE",'8aIOzEjsKx',"NhQd82hxIE");
 
 //Test
 if(mysqli_connect_errno()){
